38279297d0f4cff159594a4b675e7314d3858e95
Donzy007 <donstephco@yahoo.com>
2019-12-07 19:05:35 +0100

    Migration

--- Zelik/Models/IdentityModels.cs
+++ Zelik/Models/IdentityModels.cs
@@ -19,7 +19,9 @@ namespace Zelik.Models
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
+    { 
+        public DbSet<Customer> Customers { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
--- Zelik/Models/Movie.cs
+++ Zelik/Models/Movie.cs
@@ -9,6 +9,5 @@ namespace Zelik.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-
     }
 }
