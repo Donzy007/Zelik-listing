81f3bea925e2d3ba24a660990e50052557b5728e
Donzy007 <donstephco@yahoo.com>
2019-12-09 15:17:37 +0100

    Discount Rate

--- Zelik/Controllers/CustomersController.cs
+++ Zelik/Controllers/CustomersController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.Entity;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,28 +11,28 @@ namespace Zelik.Controllers
 {
     public class CustomersController : Controller
     {
+
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
         // GET: Customers
         public ActionResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers.Include(c=>c.MembershipType).ToList();
             return View(customers);            
         }
         
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
             if (customer == null)
                 return HttpNotFound();
             return View(customer);
         }
 
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer{Id=1, Name="Ani Joy"},
-                new Customer{Id=2, Name="Stephen T"}
-            };
-        }
+ 
     }
 }
