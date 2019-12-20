f8cf1422e1cdb7062ceb0ae162879102b05aced1
Donzy007 <donstephco@yahoo.com>
2019-12-20 13:11:50 +0100

    javascript deleting customers

--- Zelik/App_Start/MappingProfile.cs
+++ Zelik/App_Start/MappingProfile.cs
@@ -13,7 +13,13 @@ namespace Zelik.App_Start
         public MappingProfile()
         {
             Mapper.CreateMap<Customer, CustomerDto>();
-            Mapper.CreateMap<CustomerDto, Customer>();
+            //Mapper.CreateMap<CustomerDto, Customer>();
+            Mapper.CreateMap<Movie, MovieDto>();
+
+            //Dto to Domain
+            Mapper.CreateMap<CustomerDto, Customer>().ForMember(c => c.Id, opt => opt.Ignore());
+            Mapper.CreateMap<MovieDto, Movie>().ForMember(c => c.Id, opt => opt.Ignore());
+
         }
     }
 }
